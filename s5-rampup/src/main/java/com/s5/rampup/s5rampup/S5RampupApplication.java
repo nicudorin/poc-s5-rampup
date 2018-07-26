21b6b68b29389c04b47e970e08df994b927860ef
nicudorin <s.nicudorin@gmail.com>
2018-07-26 14:30:04 +0300

    create 'people' table using Migrations plugin

--- s5-rampup/src/main/java/com/s5/rampup/s5rampup/S5RampupApplication.java
+++ s5-rampup/src/main/java/com/s5/rampup/s5rampup/S5RampupApplication.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.PropertySource;
 @PropertySource("/application.properties")
 public class S5RampupApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(S5RampupApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(S5RampupApplication.class, args);
+    }
 }
